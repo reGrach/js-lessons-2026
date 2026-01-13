c96d9c39f73b68233b74cbf5f9a78403eeaab8c5
reGrach <regrachdev@yandex.ru>
2026-01-13 19:44:59 +0300

    13.01

---
+++ typesOfVar.js
@@ -0,0 +1,83 @@
+// Строка
+
+let text = "Это строка"
+console.log('text',
+            '| тип:',
+            typeof text,
+            '| значение:',
+            text);
+// Числа
+let age = 24.5;
+console.log('age',
+            '| тип:',
+            typeof age,
+            '| значение:',
+            age);
+
+let inf = Infinity;
+console.log('inf',
+            '| тип:',
+            typeof inf,
+            '| значение:',
+            inf);
+
+let divideZero = 1 / 0;
+console.log('divideZero',
+            '| тип:',
+            typeof divideZero,
+            '| значение:',
+            divideZero);
+
+let notANumber = NaN;
+console.log('notANumber',
+            '| тип:',
+            typeof notANumber,
+            '| значение:',
+            notANumber);
+
+// Булевый тип
+let itIsTrue = true;
+console.log('itIsTrue',
+            '| тип:',
+            typeof itIsTrue,
+            '| значение:',
+            itIsTrue);
+
+let itIsFalse = false;
+console.log('itIsFalse',
+            '| тип:',
+            typeof itIsFalse,
+            '| значение:',
+            itIsFalse);
+
+// Большое число
+let countOfStars = 9999999999999999999999999999999999999999n
+console.log('countOfStars',
+            '| тип:',
+            typeof countOfStars,
+            '| значение:',
+            countOfStars);
+
+// undefined
+let unknown
+console.log('unknown',
+            '| тип:',
+            typeof unknown,
+            '| значение:',
+            unknown);
+
+// null
+let empty = null;
+console.log('empty',
+            '| тип:',
+            typeof empty,
+            '| значение:',
+            empty);
+
+// Object
+let human = {};
+console.log('human',
+            '| тип:',
+            typeof human,
+            '| значение:',
+            human);
